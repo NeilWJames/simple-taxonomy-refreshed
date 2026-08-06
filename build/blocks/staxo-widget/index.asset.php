1b74c0789d19186bdacc6538c23380966ed37c10
NeilWJames <23708603+NeilWJames@users.noreply.github.com>
2026-08-06 22:31:39 +0000

    Build blocks [automated]

--- build/blocks/staxo-widget/index.asset.php
+++ build/blocks/staxo-widget/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'fc5dbd9049114785abd0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '89390e9a1f8f5c9cb0d6');
